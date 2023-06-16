fd14697ccec1e2c9cdc5652e3ae3bce418b84b4a
Duc <luuduc34@hotmail.com>
2023-06-16 11:29:43 +0200

    first commit builder package

---
+++ builder/Labyrinthe.kt
@@ -0,0 +1,6 @@
+package be.technifutur.decouvertekotlin.designPattern.builder
+
+import be.technifutur.decouvertekotlin.designPattern.factory.IElementLabyrinthe
+
+class Labyrinthe<K>(val labyMap:MutableMap<K,IElementLabyrinthe>) {
+}
---
+++ builder/Position2d.kt
@@ -0,0 +1,21 @@
+package be.technifutur.decouvertekotlin.designPattern.builder
+
+class Position2D(val line:Int, val column : Int) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Position2D
+
+        if (line != other.line) return false
+        if (column != other.column) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = line
+        result = 31 * result + column
+        return result
+    }
+}
